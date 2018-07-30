aa3f3b8c51a25a2df7832cf136355f094e4571b4
pavel <pavel.shturm.incomteh@gmail.com>
2018-07-30 12:21:05 +0400

    Исправил порядок следования урлов

--- store/store/urls.py
+++ store/store/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('catalog.urls', namespace='catalog')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
